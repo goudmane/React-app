db8d5d111f953fe822cb13641135e40f16df66a3
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-05 17:50:06 +0100

    flickity

--- src/components/section2.js
+++ src/components/section2.js
@@ -40,8 +40,14 @@ export default Section2;
  */
 import React, { Component } from "react";
 import Slider from "react-slick";
+/* import Flickity from 'flickity'; */
 
+import Flickity from "react-flickity-component";
+
+import "../styles.css";
+import "flickity/css/flickity.css";
 export default class Section2 extends Component {
+
   render() {
     const settings = {
       dots: true,
@@ -75,27 +81,10 @@ export default class Section2 extends Component {
     };
     return (
       <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-          <div>
-            <img src="https://picsum.photos/196/262"/>
-          </div>
-        </Slider>
+        <Flickity>
+          <img src="https://placeimg.com/640/480/animals" />
+          <img src="https://placeimg.com/640/480/animals" />
+        </Flickity>
       </div>
     );
   }
