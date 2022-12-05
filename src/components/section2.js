d57252313d9fa3a1bbfd6d146b498d1fa001bfb4
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-05 12:41:48 +0100

    cc

--- src/components/section2.js
+++ src/components/section2.js
@@ -47,7 +47,7 @@ export default class Section2 extends Component {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 3,
       slidesToScroll: 1
     };
     return (
