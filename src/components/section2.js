de585a29dafb4cc86359afab90b5d04af3e9d5b2
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-05 14:54:16 +0100

    edit slick

--- src/components/section2.js
+++ src/components/section2.js
@@ -47,8 +47,31 @@ export default class Section2 extends Component {
       dots: true,
       infinite: true,
       speed: 500,
+      centerMode: true,
+      centerPadding: '60px',
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
     };
     return (
       <div>
