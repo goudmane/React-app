28d9ce974e6a40f16d42f28411285c36255b6e9c
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-05 18:46:15 +0100

    syle flickity

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -8,6 +8,17 @@ const GlobalStyle = createGlobalStyle`
         margin : 10pt 20pt;
     }
 
+    .carousel-cell {
+        width: 33%;
+        height: 160px;
+        margin-right: 10px;
+      }
+    .carousel-cell.size-180 { width: 180px; }
+    .carousel-cell.size-large { width: 75%; }
+
+    .carousel-cell.is-selected {
+        /* background: #ED2; */
+    }
 `;
 
 export default GlobalStyle;
