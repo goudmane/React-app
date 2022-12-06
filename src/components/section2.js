a6a33293e2e4d1d00924159f0a83b4676041d0d7
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-06 10:28:04 +0100

    swiper slide

--- src/components/section2.js
+++ src/components/section2.js
@@ -5,6 +5,17 @@ import Flickity from "react-flickity-component";
 import "flickity/css/flickity.css";
 import RoundedBtn from "./roundedBtn";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+// import Swiper core and required modules
+import SwiperCore, {
+    Pagination
+  } from 'swiper/core';
+// install Swiper modules
+SwiperCore.use([Pagination]);
 export default class Section2 extends Component {
 
   render() {
@@ -38,7 +49,7 @@ export default class Section2 extends Component {
     return (
       <div id="section2">
         <h3 style={h3style}>THE PLACE TO BE</h3>
-        <Flickity
+        {/* <Flickity
             className={'carousel'} // default ''
             elementType={'div'} // default 'div'
             options={flickityOptions} // takes flickity options {}
@@ -49,7 +60,24 @@ export default class Section2 extends Component {
             <div class="carousel-cell"><img src="https://placeimg.com/640/480/animals" /></div>
             <div class="carousel-cell"><img src="https://placeimg.com/640/480/animals" /></div>
             <div class="carousel-cell"><img src="https://placeimg.com/640/480/animals" /></div>
-        </Flickity>
+        </Flickity> */}
+            <Swiper
+                slidesPerView={"auto"}
+                centeredSlides={true}
+                loop= {true}
+                spaceBetween={30}
+                pagination={{"clickable": true}}
+                className="mySwiper">
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
 
         <RoundedBtn marginStyle={{margin: '50px 50px 42px 50px;'}} text="Voir tous les événements" />
       </div>
