844a73ac95fd80f703e006d9ad2eb7b9f9c7548d
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-06 09:18:50 +0100

    dd

---
+++ src/components/titleText.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+
+const BtnStyle = {
+    borderRadius: '30px',
+    width: '100%',
+    background: 'var(--main-bleu)',
+    height: '45px'
+
+}
+const titleText = ({marginStyle,text}) => (
+  <div>
+    <div style={marginStyle}>
+        <Button style={BtnStyle}>{text}</Button>
+    </div>
+  </div>
+);
+
+
+
+export default titleText;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -41,30 +41,28 @@ const GlobalStyle = createGlobalStyle`
         left: -20px;
     }
 
-    .carousel-cell {
-        display: flex;
-        z-index: 5;
-        margin-top: 16px;
-        width: 172px !important;
-        height: 230px;
-    }
+    .carousel {
+        background: #FAFAFA;
+      }
 
-    .carousel-cell.is-selected {
-        z-index: 50;
-        margin-top: 0;
-        /* width: 196px !important;
-        height: 262px; */
-    }
+      .carousel-cell {
+        width: 66%;
+        height: 200px;
+        margin-right: 10px;
+        background: #8C8;
+        border-radius: 5px;
+        counter-increment: carousel-cell;
+      }
 
-    .carousel-cell > img {
-        width: 172px !important;
-        height: 230px;
-    }
-    .carousel-cell.is-selected>img{
-        /* background: #ED2; */
-
-    }
-
+      /* cell number */
+      .carousel-cell:before {
+        display: block;
+        text-align: center;
+        content: counter(carousel-cell);
+        line-height: 200px;
+        font-size: 80px;
+        color: white;
+      }
     .category-carousel.flickity-enabled.is-draggable {
         margin-bottom: 35px;
     }
@@ -103,3 +101,5 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export default GlobalStyle;
+
+
