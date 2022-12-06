51e967516e5aee1aeec9f0b2f79227f180fa1c04
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-06 03:06:52 +0100

    ass

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -67,7 +67,7 @@ const createFontFaces = () => {
   styles= `
             @font-face {
                 font-family: 'HelveticaNeue';
-                src: url(./fonts/HelveticaNeue-Regular.ttf) format("truetype");
+                src: url(./src/fonts/HelveticaNeue-Regular.ttf) format("truetype");
             }
         `
   return styles;
