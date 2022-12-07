3d6d236b03e00a094f842d22890ccec86cd09bbe
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-07 14:43:35 +0100

    d

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,29 +12,21 @@ var style = {
     left: "0",
     bottom: "0",
     height: "67.5px",
-    width: "100%",
+    width: "100vh",
+    maxWidth : '500px',
     borderTopLeftRadius:'80px',
     borderTopRightRadius:'80px',
     boxShadow: "0px -3px 6px 0px #000000",
 }
 
-var phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '60px',
-  width: '100%',
-}
+
 
 function FooterBar() {
     return (
-        <div>
-            <div style={phantom} />
-            <div style={style}>
-
-                <Button className=""><Icon name='Home' />Accueil</Button>
-                <Button className=""><Icon name='Car' />Transport</Button>
-                <Button className=""><Icon name='User' />Profile</Button>
-            </div>
+        <div style={style}>
+            <Button className=""><Icon name='Home' />Accueil</Button>
+            <Button className=""><Icon name='Car' />Transport</Button>
+            <Button className=""><Icon name='User' />Profile</Button>
         </div>
     )
 }
