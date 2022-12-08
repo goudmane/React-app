4317e9f5285f0318316c0adc6fced3cad9faf951
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-08 18:12:05 +0100

    aa

--- src/components/section4.js
+++ src/components/section4.js
@@ -3,6 +3,15 @@ import React, { Component } from "react";
 
 import Flickity from "react-flickity-component";
 import "flickity/css/flickity.css";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import img1 from '@images/s4-1.png';
+import img2 from '@images/s4-2.png';
+import img3 from '@images/s4-3.png';
+import img4 from '@images/s4-4.png';
 import RoundedBtn from "./roundedBtn";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -33,25 +42,17 @@ export default class Section4 extends Component {
     return (
       <div id="section4">
         <h3 style={h3style}>NOS MARQUES</h3>
-        {/* <Swiper
-            slidesPerView={2}
-            grid={{
-                rows: 2,
-              }}
-            centeredSlides={false}
-            loop= {true}
-            spaceBetween={20}
-            className="mySwiper">
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-            <SwiperSlide><img src="https://placeimg.com/640/480/animals" /></SwiperSlide>
-        </Swiper> */}
+
+        <Container>
+            <Row>
+                <Col><img src={img1} rounded /></Col>
+                <Col><img src={img2} rounded /></Col>
+            </Row>
+            <Row>
+                <Col><img src={img3} rounded /></Col>
+                <Col><img src={img4} rounded /></Col>
+            </Row>
+        </Container>
 
       </div>
     );
