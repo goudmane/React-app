63df91218f12f414e26862b6f39325ce36d0ac76
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-09 02:59:46 +0100

    kk

--- src/components/section3.js
+++ src/components/section3.js
@@ -85,6 +85,32 @@ const Section1 = ({ title }) => {
         paddingTop: '8px',
         height: '40px'
     }
+    const BtnSmallDiv = {
+        wordSpacing: '-3px',
+        position: 'absolute',
+        width: '100%',
+        height: '40px',
+        left: '0',
+        right: '0',
+        bottom: '301.5px',
+        opacity: '1',
+        textAlign: 'right',
+        border: 'unset',
+        paddingRight: '20px'
+    }
+    const BtnSmall = {
+        border: 'unset',
+        paddingTop: '8px',
+        width: '94px',
+        height: '40px',
+        background: '#FFFFFF 0% 0% no-repeat padding-box',
+        borderRadius: '30px',
+        textAlign: 'left',
+        font: 'normal normal bold 14px/19px Roboto',
+        letterSpacing: '0.82px',
+        color: '#303189',
+        opacity: '1'
+    }
 
     const data = useStaticQuery(graphql`
     query {
