fcb10dbea154c910b352b47aff850039e87a26ae
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-10 01:25:11 +0100

    yeh

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+const style = {
+    margin: 'auto',
+    background: 'rgb(241, 242, 243)',
+    display: 'block',
+    shapeRendering: 'auto'
+}
 const IconLoader = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(241, 242, 243); display: block; shape-rendering: auto;" width="271px" height="271px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style={style} width="271px" height="271px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
         <circle cx="50" cy="50" r="33" stroke-width="2" stroke="#303189" stroke-dasharray="51.83627878423159 51.83627878423159" fill="none" stroke-linecap="round">
-        <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50"/>
+            <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50" />
         </circle>
         <circle cx="50" cy="50" r="30" stroke-width="2" stroke="#dadada" stroke-dasharray="47.12388980384689 47.12388980384689" stroke-dashoffset="47.12388980384689" fill="none" stroke-linecap="round">
-        <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50"/>
+            <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50" />
         </circle>
     </svg>
 );
