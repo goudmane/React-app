a7927791c0bca044c089bc06d08329bbce67dba6
Goudmane Oualid <walidlioon.09@gmail.com>
2022-12-10 01:49:44 +0100

    dd

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -7,11 +7,11 @@ const style = {
     shapeRendering: 'auto'
 }
 const IconLoader = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style={style} width="271px" height="271px" viewBox="0 0 100 100" >
-        <circle cx="50" cy="50" r="33" stroke-width="2" stroke="#303189" stroke-dasharray="51.83627878423159 51.83627878423159" fill="none" stroke-linecap="round">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={style} width="271px" height="271px" viewBox="0 0 100 100" >
+        <circle cx="50" cy="50" r="33" strokeWidth="2" stroke="#303189" strokeDasharray="51.83627878423159 51.83627878423159" fill="none" strokeLinecap="round">
             <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50" />
         </circle>
-        <circle cx="50" cy="50" r="30" stroke-width="2" stroke="#dadada" stroke-dasharray="47.12388980384689 47.12388980384689" stroke-dashoffset="47.12388980384689" fill="none" stroke-linecap="round">
+        <circle cx="50" cy="50" r="30" strokeWidth="2" stroke="#dadada" strokeDasharray="47.12388980384689 47.12388980384689" strokeDashoffset="47.12388980384689" fill="none" stroke-linecap="round">
             <animateTransform attributeName="transform" type="rotate" dur="1.3513513513513513s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50" />
         </circle>
     </svg>
